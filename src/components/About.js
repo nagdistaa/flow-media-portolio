f7c1b06d5200f92d8ac9518b1a91a756fbac835e
nagdista <nagdista@gmail.com>
2025-06-13 07:31:15 +0300

    fix again

--- src/components/About.js
+++ src/components/About.js
@@ -4,16 +4,16 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="w-full py-20 p-5 md:px-52 md:py-30 flex flex-col  justify-center items-center  gap-20 ">
+    <div className="w-full py-20 p-5 md:px-52 md:py-30 flex flex-col justify-center items-center gap-20">
       {/* TEXT */}
-      <div className="w-full md:w-2/3 h-1/2 md:h-full flex flex-col justify-center items-center  gap-10 ">
-        <h1 className="font-bold text-3xl text-center  ">
+      <div className="w-full md:w-2/3 h-1/2 md:h-full flex flex-col justify-center items-center gap-10">
+        <h1 className="font-bold text-3xl text-center">
           About Flow Media
         </h1>
-        <p className="text-gray-500 text-xl text-center  leading-normal">
+        <p className="text-gray-500 text-xl text-center leading-normal">
           Flow Media is a creative advertising agency dedicated to crafting
           impactful brand experiences. We blend innovative design with strategic
-          advertising to help businesses thrive in today's dynamic market.
+          advertising to help businesses thrive in today&apos;s dynamic market.
         </p>
       </div>
       {/* BOXES */}
@@ -21,7 +21,7 @@ export default function About() {
         {aboutIcons.map((icon, idx) => (
           <div
             key={idx}
-            className="w-full md:w-1/2 py-8 flex flex-col justify-center items-center gap-5  shadow-md hover:shadow-2xl  rounded-2xl cursor-pointer "
+            className="w-full md:w-1/2 py-8 flex flex-col justify-center items-center gap-5 shadow-md hover:shadow-2xl rounded-2xl cursor-pointer"
           >
             <h1>{icon.text}</h1>
             <Image src={icon.icon} width={50} height={50} alt="icon" />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default function () {
+export default  function Footer () {
   return (
     <div className="w-full h-[20vh] flex justify-center items-center bg-[#11161E]">
       <h1 className="text-white">Flow Media © 2025 </h1>
